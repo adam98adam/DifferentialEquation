a5deebee3a018e07ccac5a00d823ab66eff49113
adam98adam <adamzebr98@gmail.com>
2021-01-18 20:54:49 +0100

    Test2

--- src/Differential.java
+++ src/Differential.java
@@ -4,7 +4,8 @@ import java.lang.Math;
 public class Differential {
 
    private static int n;
-   private static double a,h,x[];
+   private static double a,h;
+   private static double[] xs,ys,computingError;
 
    public static int getN() {
        return n;
@@ -30,7 +31,31 @@ public class Differential {
        Differential.h = h;
    }
 
-   public static String userInput() {
+    public static double[] getXs() {
+        return xs;
+    }
+
+    public static void setXs(double[] xs) {
+        Differential.xs = xs;
+    }
+
+    public static double[] getYs() {
+        return ys;
+    }
+
+    public static void setYs(double[] ys) {
+        Differential.ys = ys;
+    }
+
+    public static double[] getComputingError() {
+        return computingError;
+    }
+
+    public static void setComputingError(double[] computingError) {
+        Differential.computingError = computingError;
+    }
+
+    public static String userInput() {
        Scanner myObj = new Scanner(System.in);
        return myObj.nextLine();
    }
@@ -38,7 +63,7 @@ public class Differential {
    public static boolean checkN(String n) {
        try {
            int value = Integer.parseInt(n);
-           return value > 1 && value < 10;
+           return value >= 1 && value < 10;
        } catch (Exception e) {
            return false;
        }
@@ -63,6 +88,8 @@ public class Differential {
            n = userInput();
        }
        setN(Integer.parseInt(n));
+       allocationXYs(getN());
+       allocationComputingError(getN());
    }
 
    public static void inputA() {
@@ -77,9 +104,72 @@ public class Differential {
        setA(Double.parseDouble(a.replace(',', '.')));
    }
 
+   public static void allocationXYs(int n) {
+       setXs(new double[n+1]);
+       setYs(new double[n+1]);
+       getXs()[0] = 0.0;
+       getYs()[0] = -1.0;
+   }
+
+   public static void allocationComputingError(int n) {
+       setComputingError(new double[n]);
+   }
+
+   public static void  fillXs() {
+       setH(getA()/getN());
+       for(int i=1;i<=getN();i++) {
+           getXs()[i] = 0 + i * getH();
+       }
+    }
+
+    public static void showXs() {
+       for(int i=0;i<=getN();i++) {
+           System.out.println("x[" + i + "] = " +getXs()[i]);
+       }
+    }
+
+    public static void showYs() {
+        for(int i=0;i<=getN();i++) {
+            System.out.println("y[" + i + "] = " +getYs()[i]);
+        }
+    }
+
+    public static void showComputingError() {
+        for(int i=0;i<getN();i++) {
+            System.out.println("computingError[" + i + "] = " +getComputingError()[i]);
+        }
+    }
+
+    public static double getMaxComputingError() {
+       double max = getComputingError()[0];
+       for(int i=1;i<getN();i++) {
+           if(max < getComputingError()[i])
+               max = getComputingError()[i];
+       }
+        return max;
+    }
+
+    public static void eulerMethod() {
+       for(int i=0;i<getN();i++) {
+           getYs()[i+1] = getYs()[i] + (getH() * (getYs()[i] + (2 * Math.cos(Math.toRadians(getXs()[i])))));
+       }
+       for(int i=1;i<=getN();i++) {
+           getComputingError()[i-1] = Math.abs(getYs()[i] - (Math.sin(Math.toRadians(getXs()[i])) - Math.cos(Math.toRadians(getXs()[i]))));
+       }
+    }
+
     public static void main(String[] args) {
         inputN();
         inputA();
+        fillXs();
+        //allocationXYs(getN());
+        showXs();
+        eulerMethod();
+        showYs();
+        showComputingError();
+        System.out.println("Maximum Computing Error : " + getMaxComputingError());
+        //System.out.println("Length of xs is : " + getXs().length + " x[0]= " + getXs()[0]);
+        //System.out.println("Length of ys is : " + getYs().length + " y[0]= " + getYs()[0]);
 
     }
 
